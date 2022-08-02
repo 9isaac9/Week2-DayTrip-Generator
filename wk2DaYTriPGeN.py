3e3ca057ee6171a6f3b97127150f98fb09095f2b
Your Name <ruthmarie_rules@yahoo.com>
2022-08-02 08:32:12 -0700

    Created Reservation List

---
+++ wk2DaYTriPGeN.py
@@ -0,0 +1,22 @@
+#DAY TRIP GENERATOR
+
+
+reservation_list = ["destination", "restaurant", "transportation", "entertainment\n"]
+print(reservation_list)
+
+
+print()
+
+
+
+destination = ["Paris", "Italy", "Germany", "Ireland"]
+print(destination)
+restaurant = ["Jaques Bistro", "Brunos Olive Garden", "Schnetzels Place", "Leprechauns Pot of Gold"]
+print(restaurant)
+transportation = ["vehicle rentals", "train", "plane", "cruise liner"]
+print(transportation)
+entertainment = ["dancing", "theater", "museum","excursion"]
+print(entertainment)
+
+
+print()
