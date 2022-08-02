c592515b5f01832c18cbdfe97ce9bf4b63822617
Your Name <ruthmarie_rules@yahoo.com>
2022-08-02 09:13:36 -0700

    Created Random Selections use Random String

--- wk2DaYTriPGeN.py
+++ wk2DaYTriPGeN.py
@@ -20,3 +20,29 @@ print(entertainment)
 
 
 print()
+
+print("RANDOM SELECTIONS")
+
+print()
+#destitnation_list = ["Paris", "Italy", "Germany", "Ireland"]
+import random
+destination_list = ["Paris", "Italy", "Germany", "Ireland"]
+print(random.choice(destination_list))
+
+print()
+#restaurant_list = ["Jaques Bistro", "Brunos Olive Garden", "Schnetzels Place", "Leprechauns GoldPot"]
+import random
+restaurant_list = ["Jaques Bistro", "Brunos Olive Garden", "Schnetzels Place", "Leprechauns GoldPot"]
+print(random.choice(restaurant_list))
+
+print()
+#transportation_list = ["vehicle rentals", "train", "plane", "cruise liner"]
+import random
+transportation_list = ["vehicle rentals", "train", "plane", "cruise liner"]
+print(random.choice(transportation_list))
+
+print()
+#entertainment = ["dancing", "theater", "museum","excursion"]
+import random
+entertainment_list = ["dancing", "theater", "museum","excursion"]
+print(random.choice(entertainment_list))
